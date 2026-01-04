0ec250580d1704665178528978a2fef0965864d8
Tobechi Duru, B.Eng <tobechi.duru001@gmail.com>
2026-01-04 14:53:16 -0800

    Add React Router for navigation and update package dependencies

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,9 +1,12 @@
+import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
     <>
-     <HomePage />
+     <Routes>
+      <Route index element={<HomePage />} />
+     </Routes>
     </>
   )
 }
