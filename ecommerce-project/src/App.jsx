9593a3c4bff2bdb64e6a26c31a0e56891d99ebbf
Tobechi Duru, B.Eng <tobechi.duru001@gmail.com>
2026-01-04 18:40:07 -0800

    Add Checkout, Orders, and Tracking pages with corresponding styles; remove old HTML files

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,11 +1,17 @@
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
+import { Checkout } from './pages/Checkout';
+import { Orders } from './pages/Orders';
+import { Tracking } from './pages/Tracking';
 
 function App() {
   return (
     <>
      <Routes>
       <Route index element={<HomePage />} />
+      <Route path="Checkout" element={<Checkout />} />
+      <Route path="Orders" element={<Orders />} />
+      <Route path="Tracking" element={<Tracking />} />
      </Routes>
     </>
   )
