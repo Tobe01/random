0c8af178c60417431786f9ba2d8c45d73936db89
Tobechi Duru, B.Eng <tobechi.duru001@gmail.com>
2026-01-31 15:38:19 -0800

    updates

--- ecommerce-project/src/pages/home/products.test.jsx
+++ ecommerce-project/src/pages/home/products.test.jsx
@@ -1,6 +1,22 @@
 import { describe, it, expect } from 'vitest';
+import { render } from '@testing-library/react';
 import { Products } from './products';
 
 describe('product component', ()=>{
-  it('')
+  it('displays the product details correctly', ()=>{
+
+    const products =  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 1090,
+    keywords: ["socks", "sports", "apparel"]
+  }
+
+    render(<Products />)
+  })
 })
